b4bb189ac7445e9bb5c0269d2b9441001f043280
Willian Wamser <willian.wamser.rosa@gmail.com>
2021-09-15 23:19:22 -0300

    Removido o header do Screen Options.

--- src/Rotas.js
+++ src/Rotas.js
@@ -6,52 +6,52 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Servicos from './telas/Servicos';
 import Carrinho from './telas/Carrinho';
-import {cores} from './estilos';
+import { cores } from './estilos';
 
 const Tab = createBottomTabNavigator();
 
-export default function Rotas(){
+export default function Rotas() {
 
-    return <NavigationContainer>
-        <Tab.Navigator
-        screenOptions={{
+  return <NavigationContainer>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            tabBarIcon: ({ focused, color, size }) => {
-                let iconName;
-    
-                
-                  iconName='';
-                
-    
-                // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
-              },
-            "tabBarActiveTintColor": cores.roxo,
-            "tabBarInactiveTintColor": cores.claro,
-            "tabBarActiveBackgroundColor": cores.roxo,
-            "tabBarInactiveBackgroundColor": cores.laranja,
-            "tabBarLabelStyle": {
-              "width": "100%",
-              "flex": 4,
-              "fontWeight": "bold",
-              "fontSize": 16,
-              "lineHeight": 21,
-              "marginTop": 1,
-              "paddingTop":7,
-              "paddingBottom":5,
-              "backgroundColor": cores.laranja
-            },
-            tabBarHideOnKeyboard:true,
-            "tabBarStyle": [
-              {
-                "display": "flex"
-              },
-              null
-            ]
-          }}>
-            <Tab.Screen name="Serviços" component={Servicos}  />
-            <Tab.Screen name="Carrinho" component={Carrinho}/>
-        </Tab.Navigator>
-    </NavigationContainer>
+
+          iconName = '';
+
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        "tabBarActiveTintColor": cores.roxo,
+        "tabBarInactiveTintColor": cores.claro,
+        "tabBarActiveBackgroundColor": cores.roxo,
+        "tabBarInactiveBackgroundColor": cores.laranja,
+        "tabBarLabelStyle": {
+          "width": "100%",
+          "flex": 4,
+          "fontWeight": "bold",
+          "fontSize": 16,
+          "lineHeight": 21,
+          "marginTop": 1,
+          "paddingTop": 7,
+          "paddingBottom": 5,
+          "backgroundColor": cores.laranja
+        },
+        tabBarHideOnKeyboard: true,
+        "tabBarStyle": [
+          {
+            "display": "flex"
+          },
+          null
+        ]
+      }}>
+      <Tab.Screen name="Serviços" component={Servicos} />
+      <Tab.Screen name="Carrinho" component={Carrinho} />
+    </Tab.Navigator>
+  </NavigationContainer>
 
 }
